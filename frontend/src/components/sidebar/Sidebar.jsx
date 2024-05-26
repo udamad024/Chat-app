58bedc316c39b67f9e8b67e854eb18dbf34ab826
ashen0101 <udamad123@gmail.com>
2024-05-25 23:17:38 -0400

    changes to sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -5,14 +5,21 @@ import SearchInput from "./SearchInput";
 const Sidebar = () => {
 	return (
 		<div className='border-r border-slate-500 p-4 flex flex-col'>
-			<SearchInput />
-			<div className='divider px-3'></div>
+			<div className='flex items-center justify-between'>
+				<SearchInput />
+				<div className='relative'>
+					<LogoutButton className='relative -top-2' />
+				</div>
+			</div>
+			<div className='divider px-3 my-4'></div>
 			<Conversations />
-			<LogoutButton />
 		</div>
 	);
 };
+
 export default Sidebar;
+
+
 
 // STARTER CODE FOR THIS FILE
 // import Conversations from "./Conversations";
