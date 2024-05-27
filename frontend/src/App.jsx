d491d929ca34a30694bc133e46d92429a63b7729
Kusal Thiwanka <mhkusalthiwanka@gmail.com>
2024-05-27 04:59:29 -0400

    Add LandingPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
+import LandingPage from "./pages/landingpage/LandingPage";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import { Toaster } from "react-hot-toast";
@@ -9,9 +10,11 @@ import { useAuthContext } from "./context/AuthContext";
 function App() {
 	const { authUser } = useAuthContext();
 	return (
-		<div className='p-4 h-screen flex items-center justify-center'>
+		// <div className='p-4 h-screen flex items-center justify-center'>
+		<div>
 			<Routes>
-				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/' element={authUser ? <Navigate to={"/"} /> : <LandingPage />} />
+				<Route path='/dashboard' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
 				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
 				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
 			</Routes>
