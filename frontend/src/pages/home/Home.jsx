59777980bec3a5d997acb60ca073aef031bd7d8a
ashen0101 <udamad123@gmail.com>
2024-05-29 21:19:16 -0400

    changes to UI design

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,26 +1,50 @@
-// src/pages/home/Home.jsx
-
 import React from 'react';
 import MessageContainer from "../../components/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
+
 const Home = () => {
+    const containerStyle = {
+        display: 'flex',
+        height: '100vh',
+        padding: '5px', // Reduced padding
+        boxSizing: 'border-box',
+        margin: '10px',
+    };
+    
+
+    const sidebarStyle = {
+        width: '30%',
+        height: '100%',
+        backgroundColor: 'rgba(51, 51, 51, 0.7)', // Light black with 70% opacity
+        borderTopLeftRadius: '8px',
+        borderBottomLeftRadius: '8px',
+        boxSizing: 'border-box',
+    };
+
+    const contentStyle = {
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        borderTopRightRadius: '8px',
+        borderBottomRightRadius: '8px',
+        boxSizing: 'border-box',
+    };
+
+    const messageContainerStyle = {
+        flex: 1,
+        overflowY: 'auto',
+        boxSizing: 'border-box',
+    };
+
     return (
-        <div style={{ display: 'flex', height: '100vh' }} className='responsive-block'>
-            <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                flex: '0 0 30%',
-                height: '100%',
-                backgroundColor: 'gray',
-            }}>
+        <div style={containerStyle} className='responsive-block'>
+            <div style={sidebarStyle}>
                 <Sidebar />
-                <div style={{
-                    flex: '1 1 auto',
-                    maxHeight: '400px',
-                    overflowY: 'auto',
-                    backgroundColor: 'white',
-                }}>
+            </div>
+            <div style={contentStyle}>
+                <div style={messageContainerStyle}>
                     <MessageContainer />
                 </div>
             </div>
